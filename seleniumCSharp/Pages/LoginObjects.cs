d7d38c59648d3ac2440ada3762627d394ae2983b
Long Do <hunglong116@gmail.com>
2019-11-19 10:53:56 +0700

    refactor element

---
+++ seleniumCSharp/Pages/LoginObjects.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using seleniumCSharp.Common;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace seleniumCSharp.Pages
+{
+    public class LoginObjects
+    {
+        public static By emailTextbox = By.CssSelector("#i0116");
+        public static By nextButton = By.CssSelector("#idSIButton9");
+        public static By loginMessage = By.CssSelector("#loginMessage");
+        public static By passwordTextbox = By.CssSelector("#passwordInput");
+        public static By submitButton = By.CssSelector("#submitButton");
+        public static By errorMessage = By.CssSelector("#errorText");
+        public static By backButton = By.XPath("//*[@id='email-back-button']/span");
+        public static By loginTitle = By.CssSelector("#loginHeader > div");
+
+        public static string accountDeloitte = "longdo@deloitte.com";
+        public static string passwordDeloitte = "qwerty";
+    }
+}
--- seleniumCSharp/TestCases/LoginDeloitte.cs
+++ seleniumCSharp/TestCases/LoginDeloitte.cs
@@ -2,42 +2,42 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using seleniumCSharp.Common;
+using seleniumCSharp.Pages;
 
 namespace seleniumCSharp.TestCases
 {
     [TestFixture]
     public class LoginDeloitte:WebDriverLaunch
     {
-        String accountDeloitte = "longdo@deloitte.com";
 
         [Test, Order(1)]
         public void inputEmail()
         {
-            PageActions.input(By.CssSelector("#i0116"), accountDeloitte);
-            PageActions.click(By.CssSelector("#idSIButton9"));
-            PageActions.waitElement(By.CssSelector("#loginMessage"));
-            string temptext = PageActions.gettext(By.CssSelector("#loginMessage"));
+            PageActions.input(LoginObjects.emailTextbox, LoginObjects.accountDeloitte);
+            PageActions.click(LoginObjects.nextButton);
+            PageActions.waitElement(LoginObjects.loginMessage);
+            string temptext = PageActions.gettext(LoginObjects.loginMessage);
             Console.WriteLine(temptext);
             Assert.AreEqual(temptext, "Sign in with your Deloitte account");
         }
         [Test, Order(2)]
         public void inputPassword()
         {
-            PageActions.input(By.CssSelector("#passwordInput"), "qwerty");
-            PageActions.click(By.CssSelector("#submitButton"));
-            PageActions.waitElement(By.CssSelector("#errorText"));
-            string temptext = PageActions.gettext(By.CssSelector("#errorText"));
+            PageActions.input(LoginObjects.passwordTextbox, LoginObjects.passwordDeloitte);
+            PageActions.click(LoginObjects.submitButton);
+            PageActions.waitElement(LoginObjects.errorMessage);
+            string temptext = PageActions.gettext(LoginObjects.errorMessage);
             Console.WriteLine(temptext);
             Assert.AreEqual(temptext, "Incorrect user ID or password. Type the correct user ID and password, and try again.", "Error message is wrong!");
         }
         [Test, Order(3)]
         public void backButton()
         {
-            PageActions.waitElement(By.XPath("//*[@id='email-back-button']/span"));
-            PageActions.click(By.XPath("//*[@id='email-back-button']/span"));
-            PageActions.click(By.XPath("//*[@id='email-back-button']/span"));
-            PageActions.waitElement(By.CssSelector("#loginHeader > div"));
-            string temptext = PageActions.gettext(By.CssSelector("#loginHeader > div"));
+            PageActions.waitElement(LoginObjects.backButton);
+            PageActions.click(LoginObjects.backButton);
+            PageActions.click(LoginObjects.backButton);
+            PageActions.waitElement(LoginObjects.loginTitle);
+            string temptext = PageActions.gettext(LoginObjects.loginTitle);
             Console.WriteLine(temptext);
             Assert.AreEqual(temptext, "Sign in", "Title is wrong!");
         }

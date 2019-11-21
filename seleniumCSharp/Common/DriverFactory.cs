c7823768998fbe9c7333b82e6d335f78895e114c
Long Do <hunglong116@gmail.com>
2019-11-21 12:11:47 +0700

    Develop Extent Reports for C#

--- seleniumCSharp/Common/DriverFactory.cs
+++ seleniumCSharp/Common/DriverFactory.cs
@@ -6,6 +6,11 @@ using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Safari;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using System.IO;
+using NUnit.Framework.Interfaces;
+using System.Threading;
 
 namespace seleniumCSharp.Common
 {
@@ -13,10 +18,26 @@ namespace seleniumCSharp.Common
     public class DriverFactory
     {
         public static IWebDriver driver;
+        protected ExtentReports _extent;
+        protected ExtentTest _test;
 
         [OneTimeSetUp]
         public void startBrowser()
         {
+            try
+            {
+                _extent = new ExtentReports();
+                var dir = AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug","");
+                DirectoryInfo di = Directory.CreateDirectory(dir + "\\Test_Reports");
+                var htmlReport = new ExtentHtmlReporter(dir + "\\Test_Reports" + "\\Automation_Report" + ".html");
+                _extent.AddSystemInfo("Environment", "testphp.vulnweb");
+                _extent.AddSystemInfo("Username", "LongDo");
+                _extent.AttachReporter(htmlReport);
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
             launchDriver();
             driver.Url = "http://testphp.vulnweb.com/index.php";
         }
@@ -61,10 +82,84 @@ namespace seleniumCSharp.Common
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
         }
 
-        [OneTimeTearDown]
+        [SetUp]
+        public void BeforeTest()
+        {
+            try
+            {
+                _test = _extent.CreateTest(TestContext.CurrentContext.Test.Name);
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+        }
+        [TearDown]
+        public void AfterTest()
+        {
+            try
+            {
+                var status = TestContext.CurrentContext.Result.Outcome.Status;
+                var stacktrace = "" +TestContext.CurrentContext.Result.StackTrace + "";
+                var errorMessage = TestContext.CurrentContext.Result.Message;
+                Status logstatus;
+                switch (status)
+                {
+                    case TestStatus.Failed:
+                        logstatus = Status.Fail;
+                        string screenShotPath = Capture(driver, TestContext.CurrentContext.Test.Name);
+                        _test.Log(logstatus, "Test ended with " +logstatus + " – " +errorMessage);
+                        _test.Log(logstatus, "Snapshot below: " +_test.AddScreenCaptureFromPath(screenShotPath));
+                        break;
+                    case TestStatus.Skipped:
+                        logstatus = Status.Skip;
+                        _test.Log(logstatus, "Test ended with " +logstatus);
+                        break;
+                    default:
+                        logstatus = Status.Pass;
+                        _test.Log(logstatus, "Test ended with " +logstatus);
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+        }
+        private string Capture(IWebDriver driver, string screenShotName)
+        {
+            string localpath = "";
+            try
+            {
+                Thread.Sleep(4000);
+                ITakesScreenshot ts = (ITakesScreenshot)driver;
+                Screenshot screenshot = ts.GetScreenshot();
+                string pth = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+                var dir = AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug", "");
+                DirectoryInfo di = Directory.CreateDirectory(dir + "\\Error_Screenshots\\");
+                string finalpth = pth.Substring(0, pth.LastIndexOf("bin")) + "\\Error_Screenshots\\" +screenShotName + ".png";
+                localpath = new Uri(finalpth).LocalPath;
+                screenshot.SaveAsFile(localpath);
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+            return localpath;
+        }
+
+    [OneTimeTearDown]
         public void closeBrowser()
         {
-            driver.Quit();
+            try
+            {
+                _extent.Flush();
+            }
+             catch (Exception e)
+            {
+                throw (e);
+            }
+                driver.Quit();
         }
     }
 }
